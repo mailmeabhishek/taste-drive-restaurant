b338346e17b94ee1e32005634c78d36d47305107
Rana Abhishek <40243810+mailmeabhishek@users.noreply.github.com>
2025-08-11 11:06:50 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+let menus = document.querySelector("nav");
+let menuBtn = document.querySelector(".menu-btn");
+let closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", function () {
+  menus.classList.add("active");
+});
+
+closeBtn.addEventListener("click", function () {
+  menus.classList.remove("active");
+});
+
+console.log("Welcome to Taste Drive!");
